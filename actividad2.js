99ac633dc8fa82e16df3f7f9d8fdf1bd500d3cd9
David Alonzo <alonzovid@gmail.com>
2021-02-08 19:11:05 -0600

    Actividad 2 - Introduccion a la programacion - galileo

---
+++ actividad2.js
@@ -0,0 +1,17 @@
+var mes=10;
+mes = parseInt(prompt("Ingrese un mes:"));
+
+if(mes == 12 || mes==1 || mes == 2 ){
+  alert("es invierno");
+} else if(mes<=5 && mes>=3){
+  alert("es primavera");
+} else if(mes<=8 && mes>=6){
+  alert("es verano");
+} else if(mes<=11 && mes>=9){
+  alert("es otoño");
+} else{
+  alert("Usted ingreso un mes invalido");
+}
+
+alert("Número de carnet: 21002515 - Nombre Alumno: David Juda Alonzo Monzon");
+
